176af04ac1576d855758c1cb61b0d02a9e618ddd
Björn Edström <be@bjrn.se>
2012-11-29 20:27:55 +0100

    update xmonad conf for single screen.

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,26 @@
 import XMonad
 import XMonad.Config.Gnome
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig
+import XMonad.Hooks.SetWMName
 
-main = xmonad gnomeConfig
+main = xmonad $ gnomeConfig
        { modMask = mod4Mask
+       , startupHook = setWMName "LG3D"
        , terminal = "gnome-terminal"
-       }
+       , workspaces = myWorkspaces
+       } `additionalKeysP` myKeys
 
+myWorkspaces = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                "ten", "eleven", "thirteen"]
+
+myKeys =
+    [
+    -- other additional keys
+    ]
+    ++ -- important since ff. is a list itself, can't just put inside above list
+    [ (otherModMasks ++ "M-" ++ [key], action tag)
+         | (tag, key)  <- zip myWorkspaces "123456789"
+         , (otherModMasks, action) <- [ ("", windows . W.view) -- was W.greedyView
+                                      , ("S-", windows . W.shift)]
+    ]
